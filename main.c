a5f40a3eeb675d6fea104bae1ca631b6f99d8e9a
KinyanjuiMacharia-s <55922853+KinyanjuiMacharia-s@users.noreply.github.com>
2019-10-04 12:36:47 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	int a, b, sum;
+	
+    printf("Enter integer 1\n");
+    scanf("%d", &a);
+    
+     printf("Enter integer two\n");
+     scanf("%d", &b);
+     
+     sum = a+b;
+      printf("Answer is: %d\n", sum);
+      printf("first integer: %d\n", a);
+      printf("second integer: %d\n", b);
+    return 0;
+}
